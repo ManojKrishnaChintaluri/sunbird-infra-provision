13fda3b630c4c04ceb9222925826b84a0b8dca95
ManojCKrishna <Manoj.Chintaluri@syngenta.com>
2023-03-04 14:16:54 +0530

    issue: Update: Add s3 policies to nodes

--- terraform/aws/eks_nodes_policy_attachment.tf
+++ terraform/aws/eks_nodes_policy_attachment.tf
@@ -17,3 +17,8 @@ resource "aws_iam_role_policy_attachment" "AmazonEBSCSIDriverPolicy" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
     role    = aws_iam_role.eks_nodes_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "node_s3_policy" {
+    policy_arn = "arn:aws:iam::725876873105:policy/obsrv-eks-cluster-s3-policy"
+    role    = aws_iam_role.eks_nodes_role.name
+}
